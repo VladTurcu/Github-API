b9e9f9760ef77bca08a9b4e8af4821f296945cee
VladTurcu <turcuvlad87@gmail.com>
2017-12-17 16:26:10 +0000

    calling github api with Axios

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      github: {}
+      github: {},
+      username: 'VladTurcu',
+      page: 1,
+      per_page: 6
     };
   }
 
+  componentDidMount() {
+    Axios
+      .get(`https://api.github.com/users/${this.state.username}/repos?page=${this.state.page}&per_page=${this.state.per_page}`)
+      .then(res => this.setState({github: res.data}))
+      .catch(err => console.log(err));
+
+  }
+
   render() {
+    console.log(this.state.github);
     return (
       <h1>Hello Github</h1>
     );
